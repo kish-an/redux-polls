d6a77eb503ada030cb9c2aeb6584f48f45c6b11f
Kishan <kishan.o@hotmail.com>
2020-03-09 21:45:14 +0000

    Remove import

--- src/components/AddPoll.js
+++ src/components/AddPoll.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { handleAddPoll } from '../actions/polls';
-import { Redirect } from 'react-router-dom';
 
 class AddPoll extends Component {
     state = {
