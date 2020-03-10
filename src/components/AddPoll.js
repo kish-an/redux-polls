995a7e8ad11b88771ef6501595a3e554f82457b1
Kishan <kishan.o@hotmail.com>
2020-03-10 00:49:33 +0000

    Refactor AddPoll to use hooks

--- src/components/AddPoll.js
+++ src/components/AddPoll.js
@@ -1,96 +1,99 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { handleAddPoll } from '../actions/polls';
 
-class AddPoll extends Component {
-    state = {
+const AddPoll = ({ history }) => {
+    const [questionFields, setQuestionFields] = useState({
         question: '',
         a: '',
         b: '',
         c: '',
         d: '',
+    });
+    const dispatch = useDispatch();
+
+    const handleChange = e => {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        setQuestionFields(questionFields => ({
+            ...questionFields,
+            [name]: value,
+        }));
     }
 
-    handleChange = e => {
-        this.setState({
-            [e.target.name]: e.target.value,
-        })
-    }
-
-    handleSubmit = e => {
+    const handleSubmit = e => {
         e.preventDefault();
 
-        this.props.history.push('/');
+        history.push('/');
 
-        this.props.dispatch(handleAddPoll(this.state));
+        dispatch(handleAddPoll(questionFields));
     }
 
-    render() {
-        const { question, a, b, c, d } = this.state;
+    const { question, a, b, c, d } = questionFields;
 
-        return (
-            <form className='add-form' onSubmit={this.handleSubmit}>
-                <h3 style={{marginBottom: 5}}>What is your question?</h3>
-                <input
-                    value={question}
-                    name='question'
-                    onChange={this.handleChange}
-                    className='input'
-                    type='text'
-                />
+    return (
+        <form className='add-form' onSubmit={handleSubmit}>
+            <h3 style={{ marginBottom: 5 }}>What is your question?</h3>
+            <input
+                value={question}
+                name='question'
+                onChange={handleChange}
+                className='input'
+                type='text'
+            />
 
-                <h4>What are the options?</h4>
+            <h4>What are the options?</h4>
 
-                <label className='label' htmlFor='a'>A.</label>
-                <input
-                    value={a}
-                    name='a'
-                    id='a'
-                    onChange={this.handleChange}
-                    className='input'
-                    type='text'
-                />
+            <label className='label' htmlFor='a'>A.</label>
+            <input
+                value={a}
+                name='a'
+                id='a'
+                onChange={handleChange}
+                className='input'
+                type='text'
+            />
 
-                <label className='label' htmlFor='b'>B.</label>
-                <input
-                    value={b}
-                    name='b'
-                    id='b'
-                    onChange={this.handleChange}
-                    className='input'
-                    type='text'
-                />
+            <label className='label' htmlFor='b'>B.</label>
+            <input
+                value={b}
+                name='b'
+                id='b'
+                onChange={handleChange}
+                className='input'
+                type='text'
+            />
 
-                <label className='label' htmlFor='c'>C.</label>
-                <input
-                    value={c}
-                    name='c'
-                    id='c'
-                    onChange={this.handleChange}
-                    className='input'
-                    type='text'
-                />
+            <label className='label' htmlFor='c'>C.</label>
+            <input
+                value={c}
+                name='c'
+                id='c'
+                onChange={handleChange}
+                className='input'
+                type='text'
+            />
 
-                <label className='label' htmlFor='d'>D.</label>
-                <input
-                    value={d}
-                    name='d'
-                    id='d'
-                    onChange={this.handleChange}
-                    className='input'
-                    type='text'
-                />
+            <label className='label' htmlFor='d'>D.</label>
+            <input
+                value={d}
+                name='d'
+                id='d'
+                onChange={handleChange}
+                className='input'
+                type='text'
+            />
 
-                <button
-                    type='submit'
-                    className='btn'
-                    disabled={!question || !a || !b || !c || !d}
-                >
-                    Submit
+            <button
+                type='submit'
+                className='btn'
+                disabled={!question || !a || !b || !c || !d}
+            >
+                Submit
                 </button>
-            </form>
-        )
-    }
+        </form>
+    )
 }
 
-export default connect()(AddPoll);
+export default AddPoll;
