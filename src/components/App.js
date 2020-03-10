4206281cf1db8f7d19bcfc694d8d0f43ab561537
Kishan <kishan.o@hotmail.com>
2020-03-10 17:47:40 +0000

    Refactor App to use hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
 import Dashboard from './Dashboard';
 import LoadingBar from 'react-redux-loading';
@@ -9,40 +9,33 @@ import Nav from './Nav';
 import Leaderboard from './Leaderboard';
 import AddPoll from './AddPoll';
 
+const App = () => {
+    const isLoading = useSelector(state => state.authedUser === null);
+    const dispatch = useDispatch();
 
-class App extends Component {
-    componentDidMount() {
-        this.props.dispatch(handleInitialData());
-    }
+    useEffect(() => {
+        dispatch(handleInitialData());
+    }, [dispatch]);
 
-    render() {
-        return (
-            <Router>
-                <React.Fragment>
-                    <LoadingBar />
-                    <div className='container'>
-                        <Nav />
-                        {this.props.isLoading
-                            ? null
-                            :   <div>
-                                    <Route exact path='/' component={Dashboard} />
-                                    <Route path='/leaderboard' component={Leaderboard} />
-                                    <Route path='/polls/:id' component={Poll} />
-                                    <Route path='/add' component={AddPoll} />
-                                </div>
-                        }
-                    </div>
-                </React.Fragment>
-            </Router>
-
-        )
-    }
+    return (
+        <Router>
+            <>
+                <LoadingBar />
+                <div className='container'>
+                    <Nav />
+                    {isLoading
+                        ? null
+                        :   <>
+                                <Route exact path='/' component={Dashboard} />
+                                <Route path='/leaderboard' component={Leaderboard} />
+                                <Route path='/polls/:id' component={Poll} />
+                                <Route path='/add' component={AddPoll} />
+                            </>
+                    }
+                </div>
+            </>
+        </Router>
+    )
 }
 
-function mapStateToProps(state) {
-    return {
-        isLoading: state.authedUser === null,
-    }
-}
-
-export default connect(mapStateToProps)(App);
+export default App;
